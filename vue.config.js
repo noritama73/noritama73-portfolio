8c3cc1cac15ab45b924af3f2ca1cb5fe4664b234
noritama73 <takuma.shioiri.eiko.rbb@gmail.com>
2022-09-25 20:20:40 +0900

    init

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  assetsDir: "./",
   publicPath: "/noritama73-portfolio",
   outputDir: "docs",
 });
