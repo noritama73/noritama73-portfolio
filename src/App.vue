5f0c9951234fbe8e943521a8396ab536f0d2a611
noritama73 <takuma.shioiri.eiko.rbb@gmail.com>
2022-09-26 23:44:24 +0900

    porfileとskillのベース

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <HeaderMenu />
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HeaderMenu from "@/components/HeaderMenu.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HeaderMenu,
+  },
+});
+</script>
 
 <style>
 #app {
@@ -13,18 +22,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
